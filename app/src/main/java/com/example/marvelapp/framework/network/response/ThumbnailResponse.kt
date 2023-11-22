9b13d0560de1214dded1836a984b9589c9ac48d7
Wesley <wesleyssjanuario@gmail.com>
2023-11-22 14:15:18 -0300

    Adiciona Store no GitIgnore

--- app/src/main/java/com/example/marvelapp/framework/network/response/ThumbnailResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/ThumbnailResponse.kt
@@ -3,6 +3,7 @@ package com.example.marvelapp.framework.network.response
 import com.google.gson.annotations.SerializedName
 
 data class ThumbnailResponse(
+    @SerializedName("path")
     val path: String,
     @SerializedName("extension")//Quando quiser mudar o nome diferente do que esta na API usa essa anotação.
     val extension: String,
